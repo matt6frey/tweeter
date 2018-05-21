c820c1a2c5badb639de66c0fd6a2283f1c9be938
Matt Frey <matt6frey@gmail.com>
2018-05-21 22:45:01 +0000

    Added a tweet component on index.html

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,10 +4,9 @@ $( document ).ready(function () {
   $('textarea').on('keyup', function(event) {
     var charLength = $(this).val().length; // Get string length of tweet
     var counter = $('.counter');
+    counter.css('color', '#000'); // Text is black unless...
     if(charLength > 140) {
       counter.css('color', 'red');
-    } else {
-      counter.css('color', '#000');
     }
     counter.text(140 - charLength); // Change Character length
   });
