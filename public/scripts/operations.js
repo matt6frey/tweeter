9a29c6df4fc7fa0c9bd5bc5ab5222ad0764b5198
Matt Frey <matt6frey@gmail.com>
2018-05-24 19:19:36 +0000

    Fixed counter to reset to 140 chars on submission. Was only reseting on new tweet composition.

--- public/scripts/operations.js
+++ public/scripts/operations.js
@@ -16,6 +16,7 @@ $( document ).ready(function () {
         success: function (response) {
           renderTweets(response);
           $('textarea').val('');
+          $('.counter').text(140);
           return;
         }
       });
