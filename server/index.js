e6dccac21c0a08dd68582be3f819243be610cee4
Matt Frey <matt6frey@gmail.com>
2018-05-26 20:18:25 +0000

    Heroku app

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const {MongoClient} = require("mongodb");
-const MDB_URI =  "mongodb://localhost:27017/tweeter";
-
+const MDB_URI =  process.env.MONGODB_URI;
 
 var tweets;
 
